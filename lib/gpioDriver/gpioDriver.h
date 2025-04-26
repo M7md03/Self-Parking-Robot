c9a4334418de6ea156ac332a4f6af663d72e07c5
M7md03 <tifa1862003@gmail.com>
2025-04-26 16:02:31 +0300

    Refactor GPIO register definitions for clarity and consistency

--- lib/gpioDriver/gpioDriver.h
+++ lib/gpioDriver/gpioDriver.h
@@ -16,98 +16,60 @@
 // GPIO register base address
 #define GPIO_BASE 0x3FF44000
 
-#define GPIO_OUTPUT_REG 0x3FF44004   // GPIO 0-31 output register
-#define GPIO_OUTPUT1_REG 0x3FF44010  // GPIO 32-39 output register
+#define GPIO_OUTPUT_REG (GPIO_BASE + 0x4)    // GPIO 0-31 output register
+#define GPIO_OUTPUT1_REG (GPIO_BASE + 0x10)  // GPIO 32-39 output register
 
-#define GPIO_OUTPUT_ENABLE_REG 0x3FF44020   // GPIO 0-31 output enable register
-#define GPIO_OUTPUT_ENABLE1_REG 0x3FF4402C  // GPIO 32-39 output enable register
+#define GPIO_OUTPUT_ENABLE_REG (GPIO_BASE + 0x20)   // GPIO 0-31 output enable register
+#define GPIO_OUTPUT_ENABLE1_REG (GPIO_BASE + 0x2C)  // GPIO 32-39 output enable register
 
-#define GPIO_FUNCn_OUT_SEL_CFG_REG(n) (GPIO_BASE + 0x530 + 0x4 * n)  // Base address for GPIO function select register
+#define GPIO_FUNCn_OUT_SEL_CFG_REG(n) (GPIO_BASE + 0x530 + 0x4 * n)  // Function select register for GPIO n
 
-#define GPIO_INPUT_REG 0x3FF4403C   // GPIO 0-31 input register
-#define GPIO_INPUT1_REG 0x3FF44040  // GPIO 32-39 input register
+#define GPIO_INPUT_REG (GPIO_BASE + 0x32)   // GPIO 0-31 input register
+#define GPIO_INPUT1_REG (GPIO_BASE + 0x40)  // GPIO 32-39 input register
 
-#define GPIO_FUNCy_IN_SEL_CFG_REG(y) (GPIO_BASE + 0x4C0 + 0x130 * y)  // Base address for GPIO function select register
-
-#define IO_MUX_REG 0x3FF49000
-#define IO_MUX_GPIO36_REG (IO_MUX_REG + 0x04)
-#define IO_MUX_GPIO37_REG (IO_MUX_REG + 0x08)
-#define IO_MUX_GPIO38_REG (IO_MUX_REG + 0x0C)
-#define IO_MUX_GPIO39_REG (IO_MUX_REG + 0x10)
-#define IO_MUX_GPIO34_REG (IO_MUX_REG + 0x14)
-#define IO_MUX_GPIO35_REG (IO_MUX_REG + 0x18)
-#define IO_MUX_GPIO32_REG (IO_MUX_REG + 0x1C)
-#define IO_MUX_GPIO33_REG (IO_MUX_REG + 0x20)
-#define IO_MUX_GPIO25_REG (IO_MUX_REG + 0x24)
-#define IO_MUX_GPIO26_REG (IO_MUX_REG + 0x28)
-#define IO_MUX_GPIO27_REG (IO_MUX_REG + 0x2C)
-#define IO_MUX_GPIO14_REG (IO_MUX_REG + 0x30)
-#define IO_MUX_GPIO12_REG (IO_MUX_REG + 0x34)
-#define IO_MUX_GPIO13_REG (IO_MUX_REG + 0x38)
-#define IO_MUX_GPIO15_REG (IO_MUX_REG + 0x3C)
-#define IO_MUX_GPIO2_REG (IO_MUX_REG + 0x40)
-#define IO_MUX_GPIO0_REG (IO_MUX_REG + 0x44)
-#define IO_MUX_GPIO4_REG (IO_MUX_REG + 0x48)
-#define IO_MUX_GPIO16_REG (IO_MUX_REG + 0x4C)
-#define IO_MUX_GPIO17_REG (IO_MUX_REG + 0x50)
-#define IO_MUX_GPIO9_REG (IO_MUX_REG + 0x54)
-#define IO_MUX_GPIO10_REG (IO_MUX_REG + 0x58)
-#define IO_MUX_GPIO11_REG (IO_MUX_REG + 0x5C)
-#define IO_MUX_GPIO6_REG (IO_MUX_REG + 0x60)
-#define IO_MUX_GPIO7_REG (IO_MUX_REG + 0x64)
-#define IO_MUX_GPIO8_REG (IO_MUX_REG + 0x68)
-#define IO_MUX_GPIO5_REG (IO_MUX_REG + 0x6C)
-#define IO_MUX_GPIO18_REG (IO_MUX_REG + 0x70)
-#define IO_MUX_GPIO19_REG (IO_MUX_REG + 0x74)
-#define IO_MUX_GPIO20_REG (IO_MUX_REG + 0x78)
-#define IO_MUX_GPIO21_REG (IO_MUX_REG + 0x7C)
-#define IO_MUX_GPIO22_REG (IO_MUX_REG + 0x80)
-#define IO_MUX_GPIO3_REG (IO_MUX_REG + 0x84)
-#define IO_MUX_GPIO1_REG (IO_MUX_REG + 0x88)
-#define IO_MUX_GPIO23_REG (IO_MUX_REG + 0x8C)
-#define IO_MUX_GPIO24_REG (IO_MUX_REG + 0x90)
+#define IO_MUX_REG (GPIO_BASE + 0x5000)  // IO MUX register base address
 
 static const uint32_t GPIO_PIN_MUX_REG[] = {
-    IO_MUX_GPIO0_REG,
-    IO_MUX_GPIO1_REG,
-    IO_MUX_GPIO2_REG,
-    IO_MUX_GPIO3_REG,
-    IO_MUX_GPIO4_REG,
-    IO_MUX_GPIO5_REG,
-    IO_MUX_GPIO6_REG,
-    IO_MUX_GPIO7_REG,
-    IO_MUX_GPIO8_REG,
-    IO_MUX_GPIO9_REG,
-    IO_MUX_GPIO10_REG,
-    IO_MUX_GPIO11_REG,
-    IO_MUX_GPIO12_REG,
-    IO_MUX_GPIO13_REG,
-    IO_MUX_GPIO14_REG,
-    IO_MUX_GPIO15_REG,
-    IO_MUX_GPIO16_REG,
-    IO_MUX_GPIO17_REG,
-    IO_MUX_GPIO18_REG,
-    IO_MUX_GPIO19_REG,
-    IO_MUX_GPIO20_REG,
-    IO_MUX_GPIO21_REG,
-    IO_MUX_GPIO22_REG,
-    IO_MUX_GPIO23_REG,
-    IO_MUX_GPIO24_REG,
-    IO_MUX_GPIO25_REG,
-    IO_MUX_GPIO26_REG,
-    IO_MUX_GPIO27_REG,
-    0,
-    0,
-    0,
-    0,
-    IO_MUX_GPIO32_REG,
-    IO_MUX_GPIO33_REG,
-    IO_MUX_GPIO34_REG,
-    IO_MUX_GPIO35_REG,
-    IO_MUX_GPIO36_REG,
-    IO_MUX_GPIO37_REG,
-    IO_MUX_GPIO38_REG,
-    IO_MUX_GPIO39_REG,
+    IO_MUX_REG + 0x44,  // GPIO 0
+    IO_MUX_REG + 0x88,  // GPIO 1
+    IO_MUX_REG + 0x40,  // GPIO 2
+    IO_MUX_REG + 0x84,  // GPIO 3
+    IO_MUX_REG + 0x48,  // GPIO 4
+    IO_MUX_REG + 0x6C,  // GPIO 5
+    IO_MUX_REG + 0x60,  // GPIO 6
+    IO_MUX_REG + 0x64,  // GPIO 7
+    IO_MUX_REG + 0x68,  // GPIO 8
+    IO_MUX_REG + 0x54,  // GPIO 9
+    IO_MUX_REG + 0x58,  // GPIO 10
+    IO_MUX_REG + 0x5C,  // GPIO 11
+    IO_MUX_REG + 0x34,  // GPIO 12
+    IO_MUX_REG + 0x38,  // GPIO 13
+    IO_MUX_REG + 0x30,  // GPIO 14
+    IO_MUX_REG + 0x3C,  // GPIO 15
+    IO_MUX_REG + 0x4C,  // GPIO 16
+    IO_MUX_REG + 0x50,  // GPIO 17
+    IO_MUX_REG + 0x70,  // GPIO 18
+    IO_MUX_REG + 0x74,  // GPIO 19
+    IO_MUX_REG + 0x78,  // GPIO 20
+    IO_MUX_REG + 0x7C,  // GPIO 21
+    IO_MUX_REG + 0x80,  // GPIO 22
+    IO_MUX_REG + 0x8C,  // GPIO 23
+    IO_MUX_REG + 0x90,  // GPIO 24
+    IO_MUX_REG + 0x24,  // GPIO 25
+    IO_MUX_REG + 0x28,  // GPIO 26
+    IO_MUX_REG + 0x2C,  // GPIO 27
+    0,                  // GPIO 28
+    0,                  // GPIO 29
+    0,                  // GPIO 30
+    0,                  // GPIO 31
+    IO_MUX_REG + 0x1C,  // GPIO 32
+    IO_MUX_REG + 0x20,  // GPIO 33
+    IO_MUX_REG + 0x14,  // GPIO 34
+    IO_MUX_REG + 0x18,  // GPIO 35
+    IO_MUX_REG + 0x04,  // GPIO 36
+    IO_MUX_REG + 0x08,  // GPIO 37
+    IO_MUX_REG + 0x0C,  // GPIO 38
+    IO_MUX_REG + 0x10,  // GPIO 39
 };
 
 void gpioSetMode(uint32_t gpio_num, uint32_t mode);
